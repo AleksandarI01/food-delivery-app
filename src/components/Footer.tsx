3d47af06d8449d6f59bbe041cf3c6b793a68ac51
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-09-03 15:23:50 +0200

    responsiveness for Navbar created. simple Footer created

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,13 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 export default function Footer() {
   return (
-    <div>Footer</div>
-  )
+    <div className="h-12 md:h-24 p-4 lg:p-20 xl:p-40 text-orange-500 flex items-center justify-between">
+      <Link href="/" className="font-bold text-xl">
+        Ristorante Italiano
+      </Link>
+      <p>created with: React, Next.js etc....</p>
+    </div>
+  );
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,17 +1,48 @@
 import React from "react";
-import Menu from "./Menu";
 import Link from "next/link";
 
+import Menu from "./Menu";
+import CartInMenu from "./CartInMenu";
+
+import { AiFillPhone } from "react-icons/ai";
+
 export default function Navbar() {
+  const phoneStyle = { color: "white" };
+  // temporary user
+  const user = false;
+
   return (
-    <div className="h-12 text-orange-500 p-4 flex items-center justify-between border-b-2 border-b-orange-500 uppercase">
+    <div className="h-12 text-orange-500 p-4 flex items-center justify-between border-b-2 border-b-orange-500 uppercase md:h-24 lg:px-20 xl:px-40">
+      {/* for MD displays */}
+      <div className="hidden md:flex gap-4 flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/">Contact</Link>
+      </div>
+
       {/* Logo */}
-      <div className="text-xl">
+      <div className="text-xl md:font-bold flex-1 md:text-center">
         <Link href="/">Ristorante Italiano</Link>
       </div>
       {/* Mobile menu */}
-      <div>
+      <div className="md:hidden">
         <Menu />
+      </div>
+
+      {/* for MD displays */}
+      <div className="hidden md:flex gap-4 items-center justify-end flex-1">
+        <div className="md:absolute top-3 r-2 lg:static flex items-center gap-2 cursor-pointer bg-orange-300 px-1 rounded-md">
+          <AiFillPhone style={phoneStyle} />
+          <span>+41 076 123 56 78</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <div>
+          <CartInMenu />
+        </div>
       </div>
     </div>
   );
