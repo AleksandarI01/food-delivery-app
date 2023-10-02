588c1a661b9697ce6e9b53a287267b604446bb6e
Aleksandar Ilic <aleks.i6@hotmail.com>
2023-10-02 23:38:24 +0200

    login page design created

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,54 @@
 import React from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <div className="p-4 h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] flex items-center justify-center">
+      {/* Box */}
+      <div className="h-full shadow-2xl rounded-md flex flex-col md:flex-row md:h-[70%] md:w-full lg:w-[60%] 2xl:w-1/2">
+        {/* Image Container */}
+        <div className="relative h-1/3 w-full md:h-full md:w-1/2">
+          <Image
+            src="/login/loginBg.png"
+            alt=""
+            fill
+            className="object-cover"
+          />
+        </div>
+
+        {/* Form Container */}
+        <div className="p-10 flex flex-col gap-8 md:w-1/2">
+          <h1 className="font-bold text-xl xl:text-3xl">Welcome</h1>
+          <p>Log in with your Google or Facebook account</p>
+          <button className="flex gap-4 p-4 ring-1 ring-orange-100 rounded-md">
+            <Image
+              src="/login/google.png"
+              alt="google logo"
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span>Sign in with Google</span>
+          </button>
+          <button className="flex gap-4 p-4 ring-1 ring-blue-100 rounded-md">
+            <Image
+              src="/login/facebook.png"
+              alt="facebook logo"
+              width={20}
+              height={20}
+              className="object-contain"
+            />
+            <span>Sign in with Facebook</span>
+          </button>
+          <p className="text-sm">
+            For any problems please{" "}
+            <Link className="underline" href="/">
+              contact us
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
--- src/app/orders/page.tsx
+++ src/app/orders/page.tsx
@@ -1,5 +1,20 @@
 import React from "react";
 
 export default function OrdersPage() {
-  return <div>OrdersPage</div>;
+  return (
+    <div className="">
+      <table>
+        <thead>
+          <tr>
+            <th>Order ID</th>
+            <th>Date</th>
+            <th>Price</th>
+            <th>Products</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </table>
+    </div>
+  );
 }
